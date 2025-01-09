48ef307557357bd5168ddc074e70d30eaf46193c
Hui Shan Pan <hweeeee@192-168-1-101.tpgi.com.au>
2025-01-10 00:32:49 +1100

    test commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/NavBar/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I am renovating this site... STAY TUNED <br></br>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+                                <Route path="/contact" element={<Contact />} />  */}
+      </Routes>
     </div>
   );
 }
